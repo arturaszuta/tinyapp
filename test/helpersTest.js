34d5f2c1890b7c2a97980e0c7f40322a9073a8da
Arturas Zuta <arturaszuta@gmail.com>
2019-08-07 13:04:58 +0000

    Add Mocha/Chai testing for helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,33 @@
+const { assert } = require('chai');
+
+const users = require('../users');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = users.findByEmail("user@example.com", users.userData).id;
+    const expectedOutput = "userRandomID";
+    // Write your assert statement here
+    assert.equal(expectedOutput, user, 'The output is correct!')
+  });
+
+  it('should return undefined if user is not in the database', function() {
+    const user = users.findByEmail("user4@example.com", users.userData).id;
+    const expectedOutput = undefined;
+    // Write your assert statement here
+    assert.equal(expectedOutput, user, 'The output is correct!')
+  });
+
+});
